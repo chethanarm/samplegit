07c554c8b41b15c97d236f0eeeb44a3daf58a7a3
crm <git@gmail.com>
2022-11-14 12:09:31 +0000

    demo.java comments added

--- demo.java
+++ demo.java
@@ -1,10 +1,14 @@
 public class demo
 {
+	/*  comments addd */
 	public static void main (String args[])
 	
 	{
 		System.out.println("this is demo source file")
+
 	        System.out.println("welcome");
+
+		//comments modified
 	}
 
 
